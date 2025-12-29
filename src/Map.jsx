66190a543b2ebb00a35c39f6f0be7ad7d1592db4
AmIMaybeAnOrange <89079795+AmIMaybeAnOrange@users.noreply.github.com>
2025-12-29 12:41:42 -0400

    Remove duplicate import of leaflet.css

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,6 +1,6 @@
+import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 
 export default function HospitalMap() {
   const [position, setPosition] = useState(null);
