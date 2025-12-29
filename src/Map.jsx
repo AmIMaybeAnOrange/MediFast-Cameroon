4daf66c6657e1b260e955d936550bb56a7e673a4
AmIMaybeAnOrange <89079795+AmIMaybeAnOrange@users.noreply.github.com>
2025-12-29 16:58:34 -0400

    Change iconUrl from redMarker to redIcon

--- src/Map.jsx
+++ src/Map.jsx
@@ -14,7 +14,7 @@ const DefaultIcon = L.icon({
   iconAnchor: [12, 41],
 });
 const HospitalIcon = L.icon({
-  iconUrl: redMarker,
+  iconUrl: redIcon,
   shadowUrl: markerShadow,
   iconSize: [25, 41],
   iconAnchor: [12, 41],
