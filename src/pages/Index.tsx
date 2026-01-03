373926f17971f8c20edb268c2bc9a4f693ac3fb9
AmIMaybeAnOrange <89079795+AmIMaybeAnOrange@users.noreply.github.com>
2026-01-03 17:43:36 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import AppLayout from '@/components/AppLayout';
+import { Navigate } from "react-router-dom";
 
-const Index: React.FC = () => {
-  return <AppLayout />;
-};
+const Index = () => <Navigate to="/welcome" />;
 
 export default Index;
