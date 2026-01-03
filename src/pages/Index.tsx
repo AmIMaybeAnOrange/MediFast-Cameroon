4d2e108ca6597bb22a04096594cf411b1b057f1d
AmIMaybeAnOrange <89079795+AmIMaybeAnOrange@users.noreply.github.com>
2026-01-03 17:59:14 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import { Navigate } from "react-router-dom";
 
-const Index = () => <Navigate to="/welcome" />;
-
-export default Index;
+export default function Index() {
+  return <Navigate to="/" />;
+}
